cc096feaa932dff08ccefbcfdb24ecf30d4bae45
GulnisaN <124379300+GulnisaN@users.noreply.github.com>
2023-02-11 11:24:37 +0300

    DZ3_Gulnisa__23-01

---
+++ DZ3_Gulnisa_23-01/main.js
@@ -0,0 +1,40 @@
+//Задание 1
+// var a = prompt("1-число")
+// var b = prompt("2-число")
+
+// function min(num1 , num2) {
+//     if (num1 > num2) {
+//         alert(num2)
+//     } else if (num1 < num2) {
+//         alert(num1)
+//     } else {
+//         alert("ERROR!")
+//     }
+// }
+// min(a , b)
+
+
+//Задание 2
+// var i = prompt('напишите любое предложение !')
+// function count(text) {
+//     alert(text.length)
+// }
+// console.log(i)
+
+//Задание 3
+// var num1= Number(prompt('Первое число'))
+// var operator= prompt()
+// var num2= Number(prompt('Второе число'))
+
+// const calc = (a, c, b) => {
+//     if (c === '+') {
+//         return a + b
+//     }else if (c === '-') {
+//         return a - b
+//     }else if (c === '*') {
+//         return a * b
+//     }else if (c === '/' && b === 0 ) {
+//         return a / b
+//     }
+// }
+// alert(calc( num1 , operator , num2 ))
