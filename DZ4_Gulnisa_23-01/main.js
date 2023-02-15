15c44995e96b445c5c599ae6d3f2839808575db4
GulnisaN <124379300+GulnisaN@users.noreply.github.com>
2023-02-15 07:33:38 +0300

    DZ4_Gulnisa_23-01

---
+++ DZ4_Gulnisa_23-01/main.js
@@ -0,0 +1,52 @@
+// Задание 2
+// document.body.style.width = '500px'
+// document.body.style.height = '500px'
+// console.log(document)
+//
+// const posX = document.getElementById('posX')
+// const posY = document.getElementById('posY')
+// document.addEventListener('mouseover', (event) => {
+//     posX.innerHTML = event.screenX
+//     posY.innerHTML = event.screenY
+// })
+
+// Задание 1
+// const btn = document.querySelector('button')
+//
+// function btnclick (event) {
+//     event.preventDefault();
+//     let countClickBtn = btn.textContent;
+//     btn.textContent = parseInt(countClickBtn) +1;
+//
+// }
+// btn.addEventListener('click', btnclick)
+
+
+//Задание 3
+// const block = document.querySelector('.block')
+// console.log(block)
+// var red = prompt('Красный')
+// var yellow = prompt('Желтый')
+// var green = prompt('Зеленый')
+//
+// const color = (red, yellow, green) => {
+//     switch (red) {
+//         case block.querySelector('.red'):
+//             alert('STOP')
+//             break
+//         case block.querySelector('.yellow'):
+//             alert('WAIT')
+//             break
+//         case block.querySelector('.green'):
+//             alert('GO')
+//             break
+//
+//         default:
+//             alert('Светофор')
+//             break
+//     }
+//
+// }
+
+
+
