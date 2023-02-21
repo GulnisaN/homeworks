eb5c66b268a49256793348816d354ac3642f2eab
GulnisaN <124379300+GulnisaN@users.noreply.github.com>
2023-02-21 19:27:49 +0300

    Gulnisa

---
+++ DZ6_Gulnisa_23-01/main.js
@@ -0,0 +1,36 @@
+//Задание 1
+// function changeTxt() {
+//     const input = document.getElementById('input').value
+//     const items = ['item1', 'item2', 'item3']
+//     let newVal = ['']
+//
+//     for (let i = input.length - 1; i >= 0; i--) {
+//         if (input.trim() === '') {
+//             return false
+//         } else {
+//             newVal += input[i]
+//
+//         }
+//     }
+//     document.getElementById('output').value = newVal
+// }
+
+//Задание 2
+// let arr = ['BMW', 'Mercedes', 'Volkswagen', 'Toyota']
+//
+// const carList = document.querySelector('.car-list')
+// let list = ''
+//
+// arr.forEach(item => {
+//     list += `<li>${ item }</li>`
+// })
+// arr.forEach((item, index) => {
+//     if (item === 'Toyota') {
+//         arr[index] = 'Lexus'
+//     }
+// })
+// console.log(arr)
+//
+// carList.innerHTML = list
+
+
